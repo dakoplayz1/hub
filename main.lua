1fe06ab2e3ca06966bfbb8e2935ebe7a208f247e
dakoplayz1 <cutisjackson767@gmail.com>
2025-03-05 10:12:18 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -10,58 +10,93 @@ KeyGuardLibrary.Set({
 })
 
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local key = ""
+local key = ("/Dcorehub/key")
+local response = KeyGuardLibrary.validateDefaultKey(key)
+local premium  = KeyGuardLibrary.validatePremiumKey(key)
+if response == trueData then
+    Fluent:Notify({
+        Title = "Dcore",
+        Content = "loading free menu!",
+        Duration = 8
+    })
+    loadstring(game:HttpGet('https://github.com/dakoplayz1/hub/raw/refs/heads/main/hub.lua'))()
+    Fluent:Destroy()
+elseif premium == truedata then
+    Fluent:Notify({
+        Title = "Dcore",
+        Content = "loading premium menu!",
+        Duration = 8
+    })
+    -- loadstring for premium
+    Fluent:Destroy()
+else
+    local Window = Fluent:CreateWindow({
+        Title = "Dcore hub",
+        SubTitle = "Dcore Key",
+        TabWidth = 160,
+        Size = UDim2.fromOffset(580, 340),
+        Acrylic = true,
+        Theme = "Dark",
+        MinimizeKey = Enum.KeyCode.LeftControl
+    })
 
-local Window = Fluent:CreateWindow({
-    Title = "Dcore hub",
-    SubTitle = "Dcore Key",
-    TabWidth = 160,
-    Size = UDim2.fromOffset(580, 340),
-    Acrylic = true,
-    Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.LeftControl
-})
+    local Tabs = {
+        KeySys = Window:AddTab({ Title = "Key System", Icon = "key" }),
+    }
 
-local Tabs = {
-    KeySys = Window:AddTab({ Title = "Key System", Icon = "key" }),
-}
-
-local Entkey = Tabs.KeySys:AddInput("Input", {
-    Title = "Enter Key",
-    Description = "Enter Key Here",
-    Default = "",
-    Placeholder = "Enter key…",
-    Numeric = false,
-    Finished = false,
-    Callback = function(Value)
-        key = Value
-    end
-})
-
-local Checkkey = Tabs.KeySys:AddButton({
-    Title = "Check Key",
-    Description = "Enter Key before pressing this button",
-    Callback = function()
-        local response = KeyGuardLibrary.validateDefaultKey(key)
-        local premium  = KeyGuardLibrary.validatePremiumKey(key)
-        if response == trueData then
-           print("Key is valid")
-           loadstring(game:HttpGet('https://github.com/dakoplayz1/hub/raw/refs/heads/main/hub.lua'))()
-        elseif premium == truedata then
-            print("Key is valid")
-            
-        else
-           print("Key is invalid")
+    local Entkey = Tabs.KeySys:AddInput("Input", {
+        Title = "Enter Key",
+        Description = "Enter Key Here",
+        Default = "",
+        Placeholder = "Enter key…",
+        Numeric = false,
+        Finished = false,
+        Callback = function(Value)
+            key = Value
         end
-    end
-})
+    })
 
-local Getkey = Tabs.KeySys:AddButton({
-    Title = "Get Key",
-    Description = "Get Key here",
-    Callback = function()
-       setclipboard(KeyGuardLibrary.getLink())
-    end
-})
+    local Checkkey = Tabs.KeySys:AddButton({
+        Title = "Check Key",
+        Description = "Enter Key before pressing this button",
+        Callback = function()
+            local response = KeyGuardLibrary.validateDefaultKey(key)
+            local premium  = KeyGuardLibrary.validatePremiumKey(key)
+            if response == trueData then
+                writefile("/Dcorehub/key",key)
+                Fluent:Notify({
+                    Title = "Dcore",
+                    Content = "loading free menu!",
+                    Duration = 8
+                })
+                loadstring(game:HttpGet('https://github.com/dakoplayz1/hub/raw/refs/heads/main/hub.lua'))()
+                Fluent:Destroy()
+            elseif premium == truedata then
+                writefile("/Dcorehub/key",key)
+                Fluent:Notify({
+                    Title = "Dcore",
+                    Content = "loading premium menu!",
+                    Duration = 8
+                })
+                --loadstring goes here
+                Fluent:Destroy()
+            else
+                Fluent:Notify({
+                    Title = "Dcore",
+                    Content = "This key is invalid",
+                    Duration = 8
+                })
+            end
+        end
+    })
 
-Window:SelectTab(1)
+    local Getkey = Tabs.KeySys:AddButton({
+        Title = "Get Key",
+        Description = "Get Key here",
+        Callback = function()
+        setclipboard(KeyGuardLibrary.getLink())
+        end
+    })
+
+    Window:SelectTab(1)
+end
